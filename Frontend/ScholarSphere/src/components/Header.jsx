192a8a3edd6a4d4362352aac1d4c08c09bd772f1
JoeWarrenSoftware <joewarrensoftware@gmail.com>
2024-12-10 19:56:40 +0000

    1st group session checkin

---
+++ Frontend/ScholarSphere/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <img alt="Logo"></img>
+      <h1>Scholar Sphere Header</h1>
+      <nav>
+        <>
+        <a>User's Blogs</a>
+        <button>Logout</button>
+        </>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
