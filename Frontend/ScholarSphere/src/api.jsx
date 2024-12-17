ce20412b62641c465a3063f15b7e3753c2f45798
JoeWarrenSoftware <joewarrensoftware@gmail.com>
2024-12-17 18:39:05 +0000

    amended api server IP

--- Frontend/ScholarSphere/src/api.jsx
+++ Frontend/ScholarSphere/src/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import mockAxios from './mock/mockApi';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://127.0.0.1:8000',
 });
 
 api.interceptors.request.use((config) => {
