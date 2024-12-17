88460f84fbc44fa63a469818bb9042c0b0917710
JoeWarrenSoftware <joewarrensoftware@gmail.com>
2024-12-17 19:32:55 +0000

    Updated CORS fix for backend

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,10 +41,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'users_api',
-    'blog_api'
+    'blog_api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,11 +53,23 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Your React app's URL
+    "http://localhost:5174",  # Your React app's URL
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',  # Ensure OPTIONS is allowed
+]
+
+CORS_ALLOW_HEADERS = [
+    'content-type',  # Ensure the content-type is allowed
 ]
 
 ROOT_URLCONF = 'mysite.urls'
